cb0ce45b638fbad442d2b882d13fb2492f985ebb
phanisaimunipalli <phanisaimunipalli@gmail.com>
2024-08-04 12:42:29 -0700

    SafeWord AI - Enbaled analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SafeWordAI from "./SafeWordAI";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <SafeWordAI />
+      <Analytics />
     </div>
   );
 };
